73807191ee0c01cbb73e8ee03e1de010f2375dcb
alswo515 <67941961+alswo515@users.noreply.github.com>
2020-08-20 18:24:03 +0900

    each 메소드 추가

--- js/header.js
+++ js/header.js
@@ -1,12 +1,23 @@
 (function ($) {
 
-    $('.depth1 > li').hover(
-        function () {
-            $(this).find('.depth2').stop().slideDown(700)
-        },
-        function () {
-            $(this).find('.depth2').stop().slideUp(700)
-        }
-    )
+    // $('.depth1 > li').hover(
+    //     function () {
+    //         $(this).find('.depth2').stop().slideDown(700)
+    //     },
+    //     function () {
+    //         $(this).find('.depth2').stop().slideUp(700)
+    //     }
+    // )
+
+    $('.depth1>li').each(function () {
+        $(this).hover(
+            function () {
+                $(this).find('.depth2').stop().slideDown(500)
+            },
+            function () {
+                $(this).find('.depth2').stop().slideUp(700)
+            }
+        )
+    })
 
 })(jQuery)
